2527310f255c8633caaa116c2e868b6a79d2366b
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-09 19:10:20 -0700

    completes #4 and #5

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log('JS is working!!!!!!')
+
+function Person(fName, lName, age, img) {
+  this.fName = fName  
+  this.lName = lName
+  this.age = age
+  this.img = img
+}
+
+var Aaron = new Person("Aaron", "Villanueva", 27, "https://wallpaperbrowse.com/media/images/soap-bubble-1958650_960_720.jpg")
+var Kelsy = new Person("Kelsy", "Smith", 23, "https://wallpaperbrowse.com/media/images/cat-1285634_960_720.png")
+var Audrey = new Person("Audrey", "Chen", 32, "https://wallpaperbrowse.com/media/images/4237670-images.jpg")
+
+
